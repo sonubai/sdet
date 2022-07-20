f6b23f977fdac42d9457827f7081b3a376617296
HP <HP@LAPTOP-DR1P4N5S>
2022-07-20 17:50:08 +0530

    1

---
+++ Project1/src/test/java/org/tyss/mavenpractice/svgTest.java
@@ -0,0 +1,28 @@
+package org.tyss.mavenpractice;
+
+import org.testng.annotations.Test;
+
+public class svgTest {
+
+@Test
+public void testing() {
+	System.out.println("testing");
+	System.out.println("hello");
+}
+
+  @Test
+ 
+public void practice1() {
+	System.out.println("morning");
+}
+
+
+
+@Test
+public void practice2() {
+	System.out.println("test all");
+	System.out.println("sonu");
+}
+
+
+}
